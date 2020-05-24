2cfdbdd9ca1b07da1aee50b3b20ba599360d957c
asyncee <asyncee@users.noreply.github.com>
2020-05-24 11:06:06 +0300

    Use reconnecting consumer by default when running services.

--- src/myrabbit/runner.py
+++ src/myrabbit/runner.py
@@ -3,16 +3,21 @@ from typing import List
 from myrabbit.core.consumer.consumer import Consumer
 from myrabbit.core.consumer.consumer import ThreadedConsumer
 from myrabbit.core.consumer.listener import Listener
+from myrabbit.core.consumer.reconnecting_consumer import ReconnectingConsumer
 from myrabbit.service import Service
 
 
 def run_services(amqp_url: str, *services: Service) -> None:
     listeners: List[Listener] = sum([s.listeners for s in services], [])
-    consumer: Consumer = Consumer(amqp_url, listeners)
+    consumer = ReconnectingConsumer(
+        Consumer, consumer_kwargs=dict(amqp_url=amqp_url, listeners=listeners)
+    )
     consumer.run()
 
 
 def run_services_threaded(amqp_url: str, *services: Service) -> None:
     listeners: List[Listener] = sum([s.listeners for s in services], [])
-    consumer: Consumer = ThreadedConsumer(amqp_url, listeners)
+    consumer = ReconnectingConsumer(
+        ThreadedConsumer, consumer_kwargs=dict(amqp_url=amqp_url, listeners=listeners)
+    )
     consumer.run()
