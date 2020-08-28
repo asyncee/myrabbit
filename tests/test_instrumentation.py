5bf632933bd70834a947680b92a7553b108acaec
asyncee <asyncee@users.noreply.github.com>
2020-08-28 17:13:20 +0300

    Add service instrumentation test

--- tests/test_instrumentation.py
+++ tests/test_instrumentation.py
@@ -1,5 +1,6 @@
+import queue
 import random
-from queue import Queue
+from dataclasses import dataclass
 from time import sleep
 from typing import Callable
 
@@ -8,11 +9,16 @@ from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchExportSpanProcessor, ConsoleSpanExporter
 
-from myrabbit import Listener, PikaMessage
+from myrabbit import CommandWithMessage, Listener, PikaMessage, Service
 from myrabbit.contrib.opentelemetry.instrumentor import MyrabbitInstrumentor
 from myrabbit.core.consumer.listener import Exchange
 from myrabbit.core.consumer.listener import Queue as Q
 from myrabbit.core.publisher import make_publisher
+
+
+@dataclass
+class Command:
+    name: str
 
 
 @pytest.mark.instrumentation
@@ -43,10 +49,8 @@ def test_instrumentation(rmq_url: str, run_consumer: Callable):
     span_processor = BatchExportSpanProcessor(ConsoleSpanExporter())
     trace.get_tracer_provider().add_span_processor(span_processor)
 
-    queue: Queue = Queue()
-
     def some_callback(msg: PikaMessage) -> None:
-        queue.put(msg)
+        pass
 
     exchange = "exchange_" + str(random.randint(100000, 999999))
     queue_name = "queue_" + str(random.randint(100000, 999999))
@@ -69,3 +73,29 @@ def test_instrumentation(rmq_url: str, run_consumer: Callable):
     with run_consumer(consumer), make_publisher(rmq_url) as publisher:
         publisher.publish(exchange, "test", b"test message")
         sleep(1)
+
+
+@pytest.mark.instrumentation
+def test_service_instrumentation(
+    make_service: Callable, run_consumer: Callable, rmq_url: str
+) -> None:
+    trace.set_tracer_provider(TracerProvider())
+    MyrabbitInstrumentor().instrument_myrabbit(service_name="instrumented_service")
+    span_processor = BatchExportSpanProcessor(ConsoleSpanExporter())
+    trace.get_tracer_provider().add_span_processor(span_processor)
+
+    x: Service = make_service("X")
+    y: Service = make_service("Y")
+
+    @x.on_command(Command)
+    def x_handle_command(command: CommandWithMessage[Command]) -> None:
+        pass
+
+    from myrabbit.core.consumer import Consumer
+
+    consumer = Consumer(rmq_url, x.listeners)
+
+    with run_consumer(consumer):
+        y.send("X", Command(name="command-for-x"))
+
+    sleep(1)
